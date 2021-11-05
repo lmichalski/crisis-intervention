f48fdb120d950f40968cebefb82775ec21bbaf7f
lmichalski <l.michalski93@gmail.com>
2021-11-04 21:19:51 -0400

    merge commit
    merge home-page-rework back into main

--- src/pages/Chart.tsx
+++ src/pages/Chart.tsx
@@ -1,12 +1,10 @@
-import { FormattedMessage } from "react-intl";
 import "./Chart.scss";
 
 interface iProps {
   image: string;
-  proceedToScenario: () => void;
 }
 
-const Chart: React.FC<iProps> = ({ image, proceedToScenario }) => {
+const Chart: React.FC<iProps> = ({ image }) => {
   return (
     <div className="Chart container">
       <div className="Chart__panel panel info">
@@ -18,13 +16,6 @@ const Chart: React.FC<iProps> = ({ image, proceedToScenario }) => {
           src="/images/chart_image.png"
           alt="Client's Chart with some background information for this visit"
         />
-        <button className="Chart__proceed-button" onClick={proceedToScenario}>
-          <FormattedMessage
-            id="Chart.proceedToScenario"
-            defaultMessage="Proceed to the Scenario"
-            description="Proceed to scenario button"
-          />
-        </button>
       </div>
     </div>
   );
